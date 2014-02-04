e9f10caae95d6780111411f53a84225ea5add3cd
OrlandoLee <zongshengli@ucla.edu>
2014-02-04 13:01:52 -0800

    fix some nil bugs

--- crawler.rb
+++ crawler.rb
@@ -8,16 +8,32 @@ def processLowestLevelPage(url)
 
   page = Nokogiri::HTML(open(url))
   leftStack = page.css("div[id=left-stack]")
-  topic = leftStack.css("li[class=genre]").css("a")[0].text
-  language = leftStack.css("li[class=language]")[0].text
+  topicEntry = leftStack.css("li[class=genre]")
+  language = ""
+  topic = ""
+  podcastId = ""
+  podcastName = ""
+  artist = ""
+  link = ""
+  unless topicEntry.nil?
+    topic = topicEntry.css("a")[0].text.gsub(/[,]/, ',' => ' ') 
+  end
+  languageEntry = leftStack.css("li[class=language]")
+  unless languageEntry.nil?
+    language = languageEntry[0].text
+  end
   if language == "语言: 中文"
     podcastInfo = page.css("div").select{|podcastId| podcastId['podcast-id']}[0]
+    unless podcastInfo['podcast-id'].nil?
     podcastId = podcastInfo['podcast-id']
-    podcastName = podcastInfo['podcast-name']
-    puts podcastId
-  # puts podcastName
-    puts topic
-    puts language
+    end
+    unless podcastInfo['podcast-name'].nil?
+    podcastName = podcastInfo['podcast-name'].gsub(/[,]/, ',' => ' ') 
+    end
+    print podcastId+","
+    print podcastName+","
+    print topic+","
+    print language+","
 
 #link should compare with last time
 #if same do not update
@@ -27,12 +43,16 @@ def processLowestLevelPage(url)
     lastestEntry = contentBox.css("tbody").css("tr")[0]
     result = ""
     unless lastestEntry.nil?
-   	 artist = lastestEntry['preview-artist']
-   	 link = lastestEntry['audio-preview-url']
-    	puts artist
-    	puts link
-     	
-#    puts
+	   unless lastestEntry['preview-artist'].nil?
+   	   	artist = lastestEntry['preview-artist'].gsub(/[,]/, ',' => ' ') 
+           end
+   	   unless lastestEntry['audio-preview-url'].nil?
+   	   	link = lastestEntry['audio-preview-url']
+           end
+   	 
+    	print artist+","
+    	print link
+	puts
     	unless podcastId.nil? 
 		result = result+podcastId+","
 	    end
@@ -51,13 +71,12 @@ end
 end
 
 def processThirdLevel( topic,thirdLevelUrl)
-	File.open('result.txt', 'w') do |f2|  
-          thirdLevelPage =  Nokogiri::HTML(open(thirdLevelUrl))
+	thirdLevelPage =  Nokogiri::HTML(open(thirdLevelUrl))
+	File.open('result.txt', 'a') do |f2|  
+         
  	  firstColumn = thirdLevelPage.css("div[class='column first']").css("li")
-  	  secondColumn = thirdLevelPage.css("div[class='column']").css("li")
-  	  lastColumn = thirdLevelPage.css("div[class='column last']").css("li")
   	  firstColumn.each do |singlePage|
-		puts singlePage.text
+		#puts singlePage.text
 		url = 	singlePage.css("a")[0]["href"]
 		result = processLowestLevelPage(url)
 		if topic.nil? 
@@ -68,22 +87,13 @@ def processThirdLevel( topic,thirdLevelUrl)
 		  f2.puts result+","+topic
 		end
 	  end
-	
-          secondColumn.each do |singlePage|
-		puts singlePage.text
-		url = 	singlePage.css("a")[0]["href"]
-		result = processLowestLevelPage(url)
-		if topic.nil? 
-		  f2.puts "topic.nil"
-		elsif result.nil?
-		  
-		else	
-		  f2.puts result+","+topic
-		end
-	  end
+	  f2.close
+	end
 
-	 lastColumn.each do |singlePage|
-		puts singlePage.text
+	File.open('result.txt', 'a') do |f2| 
+	  secondColumn = thirdLevelPage.css("div[class='column']").css("li")
+          secondColumn.each do |singlePage|
+		#puts singlePage.text
 		url = 	singlePage.css("a")[0]["href"]
 		result = processLowestLevelPage(url)
 		if topic.nil? 
@@ -94,11 +104,28 @@ def processThirdLevel( topic,thirdLevelUrl)
 		  f2.puts result+","+topic
 		end
 	  end
+	  f2.close
+	end
+
+	File.open('result.txt', 'a') do |f2|
+	 lastColumn = thirdLevelPage.css("div[class='column last']").css("li")  
+	 lastColumn.each do |singlePage|
+		#puts singlePage.text
+		url = 	singlePage.css("a")[0]["href"]
+		result = processLowestLevelPage(url)
+		if topic.nil? 
+		  f2.puts "topic.nil"
+		elsif result.nil?
+		  
+		else	
+		  f2.puts result+","+topic
+		end
+	  end
+	 f2.close
 	end
 end
 
 
-begin
 rootUrl = "https://itunes.apple.com/cn/genre/podcast/id26?mt=2"
 page = Nokogiri::HTML(open(rootUrl))
 topLevelTopics = page.css("a[class=top-level-genre]")
@@ -134,5 +161,4 @@ topLevelTopics.each do |topLevelTopic|#each
 end
 
 
-end
 
